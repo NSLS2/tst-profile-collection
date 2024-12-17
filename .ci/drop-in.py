d8c41a008c12b403cb174f7795a61c243a765345
jennmald <jennmaldona@gmail.com>
2024-12-17 16:23:53 -0500

    pre-commit

--- .ci/drop-in.py
+++ .ci/drop-in.py
@@ -1,2 +1,6 @@
-print_summary(xas_demo_async(panda1, None, npoints=10_000, total_time=1, start_e=10, end_e=190))
-uid, = RE(xas_demo_async(panda1, None, npoints=10_000, total_time=1, start_e=10, end_e=190))
+print_summary(
+    xas_demo_async(panda1, None, npoints=10_000, total_time=1, start_e=10, end_e=190)
+)
+(uid,) = RE(
+    xas_demo_async(panda1, None, npoints=10_000, total_time=1, start_e=10, end_e=190)
+)
